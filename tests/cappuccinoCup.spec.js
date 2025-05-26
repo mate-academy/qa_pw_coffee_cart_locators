1499631c5d2e162419090f38d6157fe72379dc97
Inna Ianko <inna.i@mate.academy>
2025-05-26 12:46:04 +0300

    Minor style fix

--- tests/cappuccinoCup.spec.js
+++ tests/cappuccinoCup.spec.js
@@ -1,12 +1,11 @@
 import { test } from '@playwright/test';
 
 test('Cappuccino cup added to Cart', async ({ page }) => {
- /*
+  /*
   1. Open the Coffee Cart menu page https://coffee-cart.app/
   2. Click on the "Cappuccino" cup
   3. Click on the "Cart" link
   4. Wait for the URL https://coffee-cart.app/cart 
   5. Click "+" button to add one more cup of coffee to the cart
- */
-
+  */
 });
